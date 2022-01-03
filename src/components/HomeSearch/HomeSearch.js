3c985789b0043eedbbfed62642920635214ec43e
kien2572001 <kien2572001.ntk@gmail.com>
2022-01-03 15:59:34 +0700

    xong homesearch

--- src/components/HomeSearch/HomeSearch.js
+++ src/components/HomeSearch/HomeSearch.js
@@ -1,18 +1,30 @@
 import React from "react";
 import './HomeSearch.scss'
 
-class HomeSearch extends React.Component{
-    render(){
+class HomeSearch extends React.Component {
+    render() {
         return (
             <div className="home-search">
                 <div className="slogan-content">
-                    <h1>Groceries Delivered in 90 Minute</h1>
-                    <p>Get your healthy foods &nap; snacks delivered at your doorsteps all day everyday</p>
-                    <div className="search-bar">
-                        <input/>
-                        <button>Search</button>
+                    <div className="text1">
+                        <h1 class="text-2xl lg:text-4xl xl:text-5xl tracking-tight text-heading font-bold">Groceries Delivered in 90 Minute</h1>
                     </div>
-                </div>              
+                    <div className="text2">
+                        <p class="text-sm lg:text-base xl:text-lg text-heading">Get your healthy foods &amp; snacks delivered at your doorsteps all day everyday</p>
+                    </div>
+                    <>
+                        <nav class="navbar navbar-light justify-content-between fonttext">
+                            <form class="form-inline">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search your products from here" aria-label="Search" />
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                                    <i class="fas fa-search"></i>
+                                    <span>Search</span>
+                                </button>
+                            </form>
+                        </nav>
+                    </>
+                    <div className="nuller"></div>
+                </div>
             </div>
         )
     }
