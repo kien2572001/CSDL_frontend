99ea19aee7356e9bbd091929920c1cb995d50157
kien2572001 <kien2572001.ntk@gmail.com>
2022-07-29 01:51:27 +0700

    backup

--- src/components/Admin/Order/OrderDetail.js
+++ src/components/Admin/Order/OrderDetail.js
@@ -1,111 +1,174 @@
 import React from "react";
-import './OrderDetail.scss';
-import {handleGetUserInfoByCid} from '../../../services/userService'
+import "./OrderDetail.scss";
+import { handleGetUserInfoByCid } from "../../../services/userService";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 class OrderDetail extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            khachHang: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      khachHang: null,
+    };
+  }
+
+  async componentDidMount() {
+    let data = await handleGetUserInfoByCid(this.props.order.cid);
+    this.setState({
+      khachHang: data[0],
+    });
+  }
+
+  pdfGenerate = (item) => {
+    console.log(">>> check pdf", item);
+    let data = this.props.data;
+    //console.log(">>> check data", data);
+    var head = [["Product", "Quantity", "Price"]];
+    var body = [];
+    data.forEach((item) => {
+      body.push([item.title, item.quantity, "$" + item.price]);
+    });
+
+    var doc = new jsPDF();
+    doc.text(20, 20, "Order ID: " + item.orderId);
+    doc.autoTable({
+      startY: 40,
+      head: head,
+      body: body,
+      theme: "grid",
+    });
+
+    doc.autoTable({
+      head: [["Customer", " Detail"]],
+      theme: "grid",
+      didDrawCell: function (data) {
+        if (data.column.index === 0) {
+          doc.autoTable({
+            startY: data.cell.y + 2,
+
+            theme: "grid",
+            tableWidth: "wrap",
+          });
         }
-    }
+      },
+      startY: doc.autoTable.previous.finalY + 10,
+    });
+    doc.save("order.pdf");
+  };
 
-    async componentDidMount(){
-        let data = await handleGetUserInfoByCid(this.props.order.cid)
-        this.setState({
-            khachHang: data[0]
-        })
-    }
-
-    render() {
-        let arrItem = this.props.data
-        let order = this.props.order
-        console.log('Khach hang: ',this.state.khachHang)
-        return (
-            <>
-                <div className="OrderDetail__container">
-                    <div className="OrderDetail">
-                        <div className="OrderDetail__header">
-                            <h3 className="OrderDetail__header-title">ORDER ID -5ZPLFXC8</h3>
-                        </div>
-                        <div className="OrderDetail__table">
-                            <table>
-                                <colgroup>
-                                    <col style={{ width: "120px" }}></col>
-                                    <col style={{ width: "360px" }}></col>
-                                    <col ></col>
-                                    <col ></col>
-                                </colgroup>
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Product</th>
-                                        <th>Quantity</th>
-                                        <th>Price</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {arrItem?.map((item)=>{
-                                        return (
-                                            <tr>
-                                                <td><img src={item.img} alt="" className="product-img"></img></td>
-                                                <td>
-                                                    <span>{item.title}</span>
-
-                                                </td>
-                                                <td>{item.quantity}</td>
-                                                <td>{item.price}$</td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="OrderDetail__footer">
-                            <div className="Customer" >
-                                <div className="OrderDetail-customer-title">Customer</div>
-                                <div className="OrderDetail-customer">
-                                    <span className="order-text--bold">Name:</span>
-                                    <span>{this.state.khachHang?.firstName} {this.state.khachHang?.lastName}</span>
-                                </div>
-                                <div className="OrderDetail-customer">
-                                    <span className="order-text--bold">Telephone:</span>
-                                    <span>{this.state.khachHang?.phone}</span>
-                                </div>
-                                <div className="OrderDetail-address">
-                                    <span className="order-text--bold">Shipping Address:</span>
-                                    <span>{this.state.khachHang?.address}</span>
-                                </div>
-
-                            </div>
-
-                            <div className="bill">
-                                <div className="OrderDetail-bill">
-                                    <span className="OrderDetail-bill-title">Sub total</span>
-                                    <span className="OrderDetail-bill-price">${order.total}</span>
-                                </div>
-                                {/* <div className="OrderDetail-bill">
-                                    <span className="OrderDetail-bill-title">Discount</span>
-                                    <span className="OrderDetail-bill-price">$0.00</span>
-                                </div> */}
-                                <div className="OrderDetail-bill">
-                                    <span className="OrderDetail-bill-title">Delivery Fee</span>
-                                    <span className="OrderDetail-bill-price">$0.00</span>
-                                </div>
-                                <div className="OrderDetail-bill">
-                                    <span className="OrderDetail-bill-title order-text--bold">Total</span>
-                                    <span className="OrderDetail-bill-price order-text--bold">${order.total}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+  render() {
+    let arrItem = this.props.data;
+    let order = this.props.order;
+    console.log(order);
+    //console.log('Khach hang: ',this.state.khachHang)
+    return (
+      <>
+        <div className="OrderDetail__container" id="order-detail-modal">
+          <div className="OrderDetail">
+            <div className="OrderDetail__header">
+              <h3 className="OrderDetail__header-title">
+                ORDER ID - {order.orderId}
+              </h3>
+            </div>
+            <div className="OrderDetail__table">
+              <table>
+                <colgroup>
+                  <col style={{ width: "120px" }}></col>
+                  <col style={{ width: "360px" }}></col>
+                  <col></col>
+                  <col></col>
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {arrItem?.map((item) => {
+                    return (
+                      <tr>
+                        <td>
+                          <img
+                            src={item.img}
+                            alt=""
+                            className="product-img"
+                          ></img>
+                        </td>
+                        <td>
+                          <span>{item.title}</span>
+                        </td>
+                        <td>{item.quantity}</td>
+                        <td>{item.price}$</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div className="OrderDetail__footer">
+              <div className="Customer">
+                <div className="OrderDetail-customer-title">Customer</div>
+                <div className="OrderDetail-customer">
+                  <span className="order-text--bold">Name:</span>
+                  <span>
+                    {this.state.khachHang?.firstName}{" "}
+                    {this.state.khachHang?.lastName}
+                  </span>
                 </div>
+                <div className="OrderDetail-customer">
+                  <span className="order-text--bold">Telephone:</span>
+                  <span>{this.state.khachHang?.phone}</span>
+                </div>
+                <div className="OrderDetail-address">
+                  <span className="order-text--bold">Shipping Address:</span>
+                  <span>{this.state.khachHang?.address}</span>
+                </div>
+              </div>
 
-            </>
-
-        )
-
-    }
+              <div className="bill">
+                <div className="OrderDetail-bill">
+                  <span className="OrderDetail-bill-title">Order Status</span>
+                  <span className="OrderDetail-bill-price">{order.status}</span>
+                </div>
+                <div className="OrderDetail-bill">
+                  <span className="OrderDetail-bill-title">Time Schedule</span>
+                  <span className="OrderDetail-bill-price">
+                    {order.delivery}
+                  </span>
+                </div>
+                <div className="OrderDetail-bill">
+                  <span className="OrderDetail-bill-title">Sub total</span>
+                  <span className="OrderDetail-bill-price">${order.total}</span>
+                </div>
+                <div className="OrderDetail-bill">
+                  <span className="OrderDetail-bill-title">Delivery Fee</span>
+                  <span className="OrderDetail-bill-price">$0.00</span>
+                </div>
+                <div className="OrderDetail-bill">
+                  <span className="OrderDetail-bill-title order-text--bold">
+                    Total
+                  </span>
+                  <span className="OrderDetail-bill-price order-text--bold">
+                    ${order.total}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                className="down-btn"
+                onClick={() => this.pdfGenerate(order)}
+              >
+                Download
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default OrderDetail;
--- src/components/NAV/NavigatorBar.js
+++ src/components/NAV/NavigatorBar.js
@@ -1,73 +1,80 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
-import './NavigatorBar.scss';
-import LogoShop from '../../assets/images/PickBazar.png';
-import Login from '../Auth/Login';
-import { Link } from 'react-router-dom'
+import "./NavigatorBar.scss";
+import LogoShop from "../../assets/images/PickBazar.png";
+import Login from "../Auth/Login";
+import { Link } from "react-router-dom";
 
 class NavigatorBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            titleProduct: '',
-            removeSearchData: false,
-            checkDropdow: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleProduct: "",
+      removeSearchData: false,
+      checkDropdow: false,
+      y: window.scrollY,
+    };
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-        }
+  handleScroll = () => {
+    this.setState({
+      y: window.scrollY,
+    });
+  };
+
+  handleChangeSearchTitleProduct = (event) => {
+    console.log(">>> on change", this.state);
+    if (event.target.value === "") {
+      this.setState({
+        titleProduct: event.target.value,
+        removeSearchData: false,
+      });
+      return;
     }
+    this.setState({
+      titleProduct: event.target.value,
+      removeSearchData: true,
+    });
+  };
+  handleSearchTitleProduct = () => {
+    console.log(">>> check Search bar");
+  };
+  handleRemoveTitleProduct = () => {
+    console.log(">>> check remove");
+    this.setState({
+      titleProduct: "",
+      removeSearchData: false,
+    });
+  };
+  handleButtonDropdown = () => {
+    this.setState({
+      checkDropdow: !this.state.checkDropdow,
+    });
+    console.log(">>> check dropdown", this.state.checkDropdow);
+  };
 
-
-    handleChangeSearchTitleProduct = (event) => {
-        console.log(">>> on change", this.state)
-        if (event.target.value === '') {
-            this.setState({
-                titleProduct: event.target.value,
-                removeSearchData: false,
-            })
-            return;
-        }
-        this.setState({
-            titleProduct: event.target.value,
-            removeSearchData: true,
-        })
-    }
-    handleSearchTitleProduct = () => {
-        console.log(">>> check Search bar")
-    }
-    handleRemoveTitleProduct = () => {
-        console.log(">>> check remove");
-        this.setState({
-            titleProduct: "",
-            removeSearchData: false,
-        })
-
-    }
-    handleButtonDropdown = () => {
-        this.setState({
-            checkDropdow: !this.state.checkDropdow,
-        })
-        console.log('>>> check dropdown', this.state.checkDropdow)
-    }
-
-
-    render() {
-        let { titleProduct, removeSearchData, checkDropdow } = this.state;
-        //JSX
-        return (
-            <>
-                <div className="app">
-                    <header className="header">
-                        <div className="">
-                            <nav className="header__navbar">
-                                <ul className="header__navlist-list">
-                                    <li className="header__navbar-item header__navbar-item--separate">
-                                        <Link to='/' >
-                                            <img className='header__navbar-item-logo' src={LogoShop} />
-                                        </Link>
-                                    </li>
-                                    {/*  <li className="header__navbar-item">
+  render() {
+    let { titleProduct, removeSearchData, checkDropdow } = this.state;
+    //JSX
+    return (
+      <>
+        <div className="app">
+          <header className={this.state.y < 1200 ? "header" : "header-below"}>
+            <div className="">
+              <nav className="header__navbar">
+                <ul className="header__navlist-list">
+                  <li className="header__navbar-item header__navbar-item--separate">
+                    <Link to="/">
+                      <img
+                        className="header__navbar-item-logo"
+                        src={LogoShop}
+                      />
+                    </Link>
+                  </li>
+                  {/*  <li className="header__navbar-item">
                                         <button className='header__navbar-item-btn '
                                             onClick={() => this.handleButtonDropdown()}>
                                             <i className="far fa-lemon "></i>
@@ -97,60 +104,55 @@ class NavigatorBar extends Component {
                                             </li>
                                         </ul>
                                     </li> */}
-                                </ul>
+                </ul>
 
-                                <ul className="header__navlist-list">
-                                    <li className="header__navbar-item">
-                                        <Link to={'/'} className="header__navbar-item-link"><span>Shops</span></Link>
-                                    </li>
-                                    <li className="header__navbar-item">
-                                        <a href="" className="header__navbar-item-link">
-                                            <span>Offers</span>
-                                        </a>
-                                    </li>
-                                    <li className="header__navbar-item header__navbar-item--separate">
+                <ul className="header__navlist-list">
+                  <li className="header__navbar-item">
+                    <Link to={"/"} className="header__navbar-item-link">
+                      <span>Shops</span>
+                    </Link>
+                  </li>
+                  <li className="header__navbar-item">
+                    <a href="" className="header__navbar-item-link">
+                      <span>Offers</span>
+                    </a>
+                  </li>
+                  <li className="header__navbar-item header__navbar-item--separate">
+                    <Link to={"/faq"} className="header__navbar-item-link">
+                      <span>FAQ</span>
+                    </Link>
+                  </li>
+                  <li className="header__navbar-item ">
+                    <Link to={"/contact"} className="header__navbar-item-link">
+                      <span>Contact</span>
+                    </Link>
+                  </li>
+                  <li className="header__navbar-item text--strong">
+                    {/* <button type="button" className="btn btn-danger" style={{width: '50px',height: '30px'}} onClick={()=>this.handleShowLogin()} >Join</button> */}
+                    <Login />
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </header>
 
-                                        <Link to={'/faq'} className="header__navbar-item-link"><span>FAQ</span></Link>
+          <div className="container"></div>
 
-                                    </li>
-                                    <li className="header__navbar-item ">
-                                        <Link to={'/contact'} className="header__navbar-item-link" >
-                                            <span>Contact</span>
-                                        </Link>
-                                    </li>
-                                    <li className="header__navbar-item text--strong">
-                                        {/* <button type="button" className="btn btn-danger" style={{width: '50px',height: '30px'}} onClick={()=>this.handleShowLogin()} >Join</button> */}
-                                        <Login />
-                                    </li>
-
-                                </ul>
-                            </nav>
-                        </div>
-
-                    </header>
-
-                    <div className="container">
-
-                    </div>
-
-                    <footer className="footer">
-
-                    </footer>
-                </div >
-            </>
-        )
-    }
+          <footer className="footer"></footer>
+        </div>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        language: state.app.language
-    };
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavigatorBar);
